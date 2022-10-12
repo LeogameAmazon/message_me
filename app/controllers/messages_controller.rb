5cd3b33161b9d369619ea261f558ddd1bd4d32af
LeogameAmazon <leogame@amazon.com>
2022-10-12 12:03:04 -0500

    Add method to coradcast messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,18 @@
 class MessagesController < ApplicationController
-    before_action :require_user
-  
-    def create
-      message = current_user.messages.build(message_params)
-      if message.save
-        redirect_to root_path
-      end
+  before_action :require_user
+
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                    foo: message.body
     end
-  
-    private
-  
-    def message_params
-      params.require(:message).permit(:body)
-    end
-  
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+end
